88775d98406f1083e9b0fd794a5461dc245c1087
Amrut <amrutpanda7789@gmail.com>
2020-09-01 22:02:40 +0530

    ADDED: Code to add multiple person into the addressbook

--- AddressBookService.cpp
+++ AddressBookService.cpp
@@ -1,54 +1,94 @@
 #include <iostream>
+#include <vector>
 #include "PersonDetails.cpp"
 using namespace std;
 
 class AddressBook {
 
-	Person* person;
+	vector<Person> personList;
 
 	public:
 	void addPerson(Person person) {
 
-		this -> person = new Person(person);
+		personList.push_back(person);
 	}
 
 	void editPerson() {
 
-		cout << "Enter address: " << endl;
-		string address;
-		cin >> address;
-		person -> setAddress(address);
+		bool flag = false;
+		string firstName, lastName;
+		vector<Person>::iterator person;
 
-		cout << "Enter city: " << endl;
-		string city;
-		cin >> city;
-		person -> setCity(city);
+		cout << "Enter first name of person you want to edit: ";
+		cin >> firstName;
+		cout << "Enter last name of person you want to edit: ";
+		cin >> lastName;
 
-		cout << "Enter state: " << endl;
-		string state;
-		cin >> state;
-		person -> setState(state);
+		for(person = personList.begin(); person != personList.end(); person++) {
+			if(person -> getFirstName() == firstName && person -> getLastName() == lastName) {
+				flag = true;
 
-		cout << "Enter phonenumber: " << endl;
-		string phoneNumber;
-		cin >> phoneNumber;
-		person -> setPhoneNumber(phoneNumber);
+				cout << "Enter address: " << endl;
+				string address;
+				cin >> address;
+				person -> setAddress(address);
 
-		cout << "Enter zipcode: " << endl;
-		string zipCode;
-		cin >> zipCode;
-		person -> setZipCode(zipCode);
+				cout << "Enter city: " << endl;
+				string city;
+				cin >> city;
+				person -> setCity(city);
+
+				cout << "Enter state: " << endl;
+				string state;
+				cin >> state;
+				person -> setState(state);
+
+				cout << "Enter phonenumber: " << endl;
+				string phoneNumber;
+				cin >> phoneNumber;
+				person -> setPhoneNumber(phoneNumber);
+
+				cout << "Enter zipcode: " << endl;
+				string zipCode;
+				cin >> zipCode;
+				person -> setZipCode(zipCode);
+			}
+		}
+
+		if(flag != true)
+			cout << "No record found" << endl;
 	}
 
 	void deletePersonDetails() {
 
-		delete person;
-		cout << "Person details are successfully deleted" << endl;
+		bool flag = false;
+		string firstName, lastName;
+		vector<Person> :: iterator person;
+
+		cout << "\nEnter first name : ";
+		cin >> firstName;
+		cout << "Enter last name : ";
+		cin >> lastName;
+
+		for(person = personList.begin();  person != personList.end(); person++){
+			if(person -> getFirstName() == firstName && person -> getLastName() == lastName){
+				flag = true;
+
+				personList.erase(person);
+				cout << "Person successfully deleted from addressbook" << endl;
+				break;
+			}
+		}
+
+		if (flag != 1)
+			cout << "No record found" << endl;
 	}
 
 	void displayDetails() {
 
-		person -> display();
-	}
+		for(Person person : personList) {
 
+			person.display();
+		}
+	}
 };
